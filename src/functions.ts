72e465cd2d367a60d6ec4fb9ce47a4a67a817e93
Vinod Rajbhar <vinod.r@vcloudx.com>
2024-02-01 11:30:16 +0530

    first commit

---
+++ src/functions.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import * as EnxRtc from "./EnxRtc.js"
+import { ConstraintsProps } from "./types.js"
+
+
+
+export function connect(authToken: string, { audio, video }: ConstraintsProps) {
+    // EnxRtc.joinRoom();
+    console.log('connected..')
+}
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type  ConstraintsProps = {
+    audio: boolean,
+    video: boolean,
+}
